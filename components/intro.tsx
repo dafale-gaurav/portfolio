4cb296708a33ba19bff2db23c45f59098a0993dc
dafale-gaurav <157214035+dafale-gaurav@users.noreply.github.com>
2026-02-05 15:41:41 +0530

    Update image alt text from 'Ricardo portrait' to 'Gaurav Dafale'

--- components/intro.tsx
+++ components/intro.tsx
@@ -33,7 +33,7 @@ export default function Intro() {
           >
             <Image
               src="https://images.unsplash.com/photo-1734608782665-5fba43ee4775?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Ricardo portrait"
+              alt="Gaurav Dafale"
               width="192"
               height="192"
               quality="95"
